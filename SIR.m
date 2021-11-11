8e726abb7397520bc5ee8ed1b31a47de8dfa603e
StokicDusan <stokicdusan@gmail.com>
2021-11-11 15:04:56 +0100

    add plot for peaks

--- SIR.m
+++ SIR.m
@@ -1,6 +1,7 @@
 % In the SIR model we have 3 epidemiological classes: Susceptible, Infected and Removed individuals.
 % SIR.m - we are just looking at a static population.
 clear all,close all
+pkg load signal
 dt=1;
 N=10^6;			% The total population
 beta=0.25;		% Number of contacts per day that are sufficient to spread the disease
@@ -39,6 +40,20 @@ hold on
 
 figure 3,
 plot(y,"linewidth",1.5);
+xlabel('Day',"fontsize",15)
+ylabel('Rrevalence',"fontsize",15)
+grid on
+hold on
+
+[pks,loc]=findpeaks(y);
+[pk ipk]=max(pks)
+maxI=pk			% infected population at the peak of the epidemic
+Tstar=loc(ipk)	% time where the epidemic reaches its peak
+Rinf=z(end)	% removed population after the epidemic
+
+figure 4,
+plot(vreme,y,"linewidth",1.5,
+	 loc,pks,"go","linewidth",2);
 xlabel('Day',"fontsize",15)
 ylabel('Rrevalence',"fontsize",15)
 grid on
