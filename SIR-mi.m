5fbfd7996945419f224f3aa428fdf1bb4a10b6c1
StokicDusan <stokicdusan@gmail.com>
2021-11-11 14:30:09 +0100

    change the equasion a little bit

--- SIR-mi.m
+++ SIR-mi.m
@@ -1,6 +1,6 @@
-clear all,close all, clc
-N=10^6;
+clear all,close all
 dt=1;
+N=10^6;
 beta=0.77;
 gamma=1/10;
 I(1)=10;
@@ -9,8 +9,8 @@ S(1)=N-I(1);
 mi=0.00356;
 
 for n=2:1000
-	I(n) = I(n-1)*(1+dt*(beta*(S(n-1)/N)-gamma-mi));
-    S(n) = S(n-1)+dt*(mi*N - beta*((S(n-1)*I(n-1))/N)-mi*S(n-1));
+	I(n) = I(n-1)+dt*I(n-1)*(beta*(S(n-1)/N)-gamma-mi);
+    S(n) = S(n-1)+dt*(mi*N-beta*((S(n-1)*I(n-1))/N)-mi*S(n-1));
 endfor
 
 x=S/N;
