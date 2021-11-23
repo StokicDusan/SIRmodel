aec1f4c25569d8555d27a42bc6b127ec39cf296e
StokicDusan <stokicdusan@gmail.com>
2021-11-23 12:39:59 +0100

    displaying peak coordinates

--- SIR-mi.m
+++ SIR-mi.m
@@ -3,11 +3,11 @@
 clear all,close all
 pkg load signal
 dt=1;
-N=10^6;			% The total population
+N=10^6;		% The total population
 beta=0.25;		% Number of contacts per day that are sufficient to spread the disease
 gamma=1/10;		% Fraction of the infected group that will recover during any given day
 I(1)=10;		% I(0) - initial number of infected individuals
-S(1)=N-I(1);	% S(0) - initial number of susceptible individuals
+S(1)=N-I(1);		% S(0) - initial number of susceptible individuals
 mi=0.00356;		% birth rate and mortality rate 
 
 % I(n) - number of infected individuals
@@ -23,11 +23,9 @@ z=1-x-y;		% z - removed fraction of the population
 
 vreme=1:1:2000;
 figure 1, 
-plot(vreme,y,"r","linewidth",1.5,
-	vreme,z,"g","linewidth",1.5,
-	vreme,x,"b","linewidth",1.5);
+plot(vreme,y,"r","linewidth",1.5,vreme,z,"g","linewidth",1.5,vreme,x,"b","linewidth",1.5);
 leg=legend("I","R","S")
-set(leg,"fontsize",17);
+set(leg,"fontsize",17)
 axis([0 500 0 1])
 grid on
 hold on
@@ -47,14 +45,14 @@ grid on
 hold on
 
 [pks,loc]=findpeaks(y);
-[pk ipk]=max(pks)
-maxI=pk			% infected population at the peak of the epidemic
-Tstar=loc(ipk)	% time where the epidemic reaches its peak
+[pk, ipk]=max(pks);
+maxI=pk		% infected population at the peak of the epidemic
+Tstar=loc(ipk)		% time where the epidemic reaches its peak
 Rinf=z(end)		% removed population after the epidemic
 
+disp([pks,loc])
 figure 4,
-plot(vreme,y,"linewidth",1,
-	 loc,pks,"go","linewidth",2);
+plot(vreme,y,"linewidth",1,loc,pks,"go","linewidth",2);
 xlabel('Day',"fontsize",17)
 ylabel('Prevalence',"fontsize",17)
 grid on
